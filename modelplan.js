f5c9402552bc12f0cdc224f394c5ff1d928b0a4c
Seper <svvenfen@gmail.com>
2024-11-18 01:43:16 -0800

    Third Commit

--- modelplan.js
+++ modelplan.js
@@ -4,54 +4,30 @@ const course = document.querySelectorAll(".course");
 
 const app = document.querySelector("body");
 
+app.onload = function restoreSession() {
+  for (let i = 0; i < checkmark.length; i++) {
+    checkmark[i].innerHTML = localStorage.getItem(i);
 
-app.onload = function restoreSession(){
-
-    for (let i = 0; i < checkmark.length; i++) {
-
-        checkmark[i].innerHTML = localStorage.getItem(i);
-
-        if (checkmark[i].innerHTML == "✔") {
-
-            course[i].style.textDecoration = "line-through"
-
-            course[i].style.textDecorationThickness = "2px"
-
-        }
-
+    if (checkmark[i].innerHTML == "✔") {
+      course[i].style.textDecoration = "line-through";
+      course[i].style.textDecorationThickness = "2px";
     }
+  }
+};
 
-}
+for (let i = 0; i < checkmark.length; i++) {
+  checkmark[i].addEventListener("click", () => {
+    if (checkmark[i].innerHTML != "✔") {
+      checkmark[i].innerHTML = "✔";
 
+      course[i].style.textDecoration = "line-through";
+      course[i].style.textDecorationThickness = "2px";
 
-for (let i=0; i < checkmark.length; i++) {
-
-    checkmark[i].addEventListener("click", () => {
-
-        
-
-        if (checkmark[i].innerHTML != "✔") {
-
-            checkmark[i].innerHTML = "✔"
-
-            course[i].style.textDecoration = "line-through";
-
-            course[i].style.textDecorationThickness = "2px";
-
-            localStorage.setItem(i, checkmark[i].innerHTML);
-
-        }
-
-        else {
-
-            checkmark[i].innerHTML = ""
-
-            course[i].style.textDecoration = ""
-
-            localStorage.setItem(i, "")
-
-        }
-
-    })
-
+      localStorage.setItem(i, checkmark[i].innerHTML);
+    } else {
+      checkmark[i].innerHTML = "";
+      course[i].style.textDecoration = "";
+      localStorage.setItem(i, "");
+    }
+  });
 }
